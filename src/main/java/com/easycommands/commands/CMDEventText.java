a576e68689b93a07a2de41b222b0ec6ecf5b0a23
EndMy5uffering <havemph@hotmail.de>
2022-03-19 00:58:09 +0100

    Added interactable text

--- src/main/java/com/easycommands/commands/CMDEventText.java
+++ src/main/java/com/easycommands/commands/CMDEventText.java
@@ -7,21 +7,47 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 
 public class CMDEventText {
-    public TextComponent getInteractCommand(Player p, String displayText, String value, ClickEvent.Action type, ChatColor c, boolean bold, String font) {
-		TextComponent message = new TextComponent(displayText);
+
+    public static TextComponent getInteractComponent(String displayText, String value, ClickEvent.Action type, ChatColor c, boolean bold, String font) {
+		TextComponent message = getTextComponent(displayText, c, bold);
 		message.setClickEvent(new ClickEvent(type, value));
-		message.setColor(c);
-		message.setBold(bold);
-		if(font != null) message.setFont(font);
-		//p.spigot().sendMessage(message);
 		return message;
 	}
 
-	public TextComponent getInteractCommand(Player p, String displayText, String value, ClickEvent.Action type, ChatColor c) {
-		return getInteractCommand(p, displayText, value, type, c, false, null);
+    public static TextComponent getInteractComponent(String displayText, String value, ClickEvent.Action type, ChatColor c, boolean bold) {
+		return getInteractComponent(displayText, value, type, c, bold, null);
 	}
 
-	public TextComponent getInteractCommand(Player p, String displayText, String value, ClickEvent.Action type) {
-		return getInteractCommand(p, displayText, value,type, ChatColor.WHITE, false, null);
+	public static TextComponent getInteractComponent(String displayText, String value, ClickEvent.Action type, ChatColor c) {
+		return getInteractComponent(displayText, value, type, c, false, null);
 	}
+
+	public static TextComponent getInteractComponent(String displayText, String value, ClickEvent.Action type) {
+		return getInteractComponent(displayText, value,type, ChatColor.WHITE, false, null);
+	}
+
+	public static TextComponent getTextComponent(String text, ChatColor c, boolean bold, String font){
+		TextComponent message = new TextComponent(text);
+		message.setColor(c);
+		message.setBold(bold);
+		if(font != null) message.setFont(font);
+		return message;
+	}
+
+	public static TextComponent getTextComponent(String text, ChatColor c, boolean bold){
+		return getTextComponent(text, c, bold, null);
+	}
+
+	public static TextComponent getTextComponent(String text, ChatColor c){
+		return getTextComponent(text, c, false);
+	}
+
+	public static TextComponent getTextComponent(String text){
+		return getTextComponent(text, ChatColor.WHITE);
+	}
+
+	public static void sendEventMessage(Player p, TextComponent... m){
+		p.spigot().sendMessage(m);
+	}
+
 }
