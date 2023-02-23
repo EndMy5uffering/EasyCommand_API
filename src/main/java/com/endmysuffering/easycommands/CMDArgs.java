9a8ee5c60bdf81ae4ba7b01b8ef7838fe4a5ce5d
EndMy5uffering <havemph@hotmail.de>
2023-02-23 13:57:20 +0100

    Added utility functions to CMDArgs

--- src/main/java/com/endmysuffering/easycommands/CMDArgs.java
+++ src/main/java/com/endmysuffering/easycommands/CMDArgs.java
@@ -1,5 +1,7 @@
 package com.endmysuffering.easycommands;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.command.Command;
@@ -12,6 +14,7 @@ public class CMDArgs {
     private String str;
     private String[] args;
     private Map<String, String> wildCards;
+    private interface AlertFunc{ public void call(String wildCard); }
 
     public CMDArgs(CommandSender sender, Command cmd, String str, String[] args, Map<String,String> wildCards) {
         this.sender = sender;
@@ -43,6 +46,50 @@ public class CMDArgs {
 
     public boolean hasWildCard(String name){
         return this.wildCards.get(name) != null && this.wildCards.get(name) != "";
+    }
+
+    /**
+     * Function to check for multiple wildcards at once.
+     * @param names names of wildcards
+     * @return true if all wildcards in names are present false if one or more are missing
+     */
+    public boolean hasWildCards(String... names){
+        if(names.length == 0) return false;
+        boolean exists = true;
+        for(String name : names){
+            exists &= hasWildCard(name);
+        }
+        return exists;
+    }
+
+    /**
+     * Checks if all wildcards are present and if one or more are missing calls the alert function.
+     * @param func Alert function that is called for each missing wildcard.
+     * @param names names of wildcards.
+     * @return true if all wildcards in names are present false if one or more are missing
+    */
+    public boolean checkWildCardsAndAlert(AlertFunc func, String... names){
+        if(names.length == 0) return false;
+        boolean exists = true;
+        for(String name : names){
+            boolean res = hasWildCard(name);
+            if(!res) func.call(name);
+            exists &= res;
+        }
+        return exists;
+    }
+
+    /**
+     * Checks if all wildcards are present and if one or more are missing calls the alert function.
+     * @param names names of wildcards.
+     * @return true if all wildcards in names are present false if one or more are missing
+    */
+    public String[] checkWildCardsAndCollect(String... names){
+        List<String> res = new ArrayList<String>();
+        for(String name : names){
+            if(!hasWildCard(name)) res.add(name);
+        }
+        return res.toArray(new String[0]);
     }
 
     public int getWildCardInt(String name){
